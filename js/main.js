b465ad68eefcf415030a38a01e8e4e57d5aa7439
Quinton Jason <quinton.jason@gmail.com>
2025-10-23 09:17:50 -0500

    add click to the sunflower

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 // JS scripts placed here
+const sunflower = document.querySelector('.sunflower-wrapper');
+
+sunflower.addEventListener('click', () => {
+    sunflower.classList.toggle('grow');
+});
