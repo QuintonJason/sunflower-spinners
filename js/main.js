8c3ca1553c0f849d4128f3f950ee3e568ab2c48d
Quinton Jason <quinton.jason@gmail.com>
2025-10-30 12:26:30 -0500

    add gsap

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ const sunflower = document.querySelector('.sunflower-wrapper');
 sunflower.addEventListener('click', () => {
     sunflower.classList.toggle('grow');
 });
+
+// gsap.from(".sunflower-stem", { duration: 1, x: -2000, rotation: 360 });
+
+const tl = gsap.timeline();
+
+tl.to(".sunflower-stem", { duration: 1, x: 100 })
+  .to(".sunflower-top", { duration: 1, y: 100 })
+  .to(".sunflower-top img", { duration: 1, rotation: 360 });
